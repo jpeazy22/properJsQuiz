e16c94a0e46604d0de5fae7336a13e2b98a483a7
Joanie Smeester <joaniesmeester@Joanies-MacBook-Pro-2.local>
2017-01-11 15:10:15 -0700

    #1 added submit button, working on scoring functionality

--- main.js
+++ main.js
@@ -11,26 +11,26 @@ var quiz = [
 		choices: ["simple cuboidal", "simple squamous", "transitional", "simple columnar"],
 		correctAnswer:3
 	},
-	// {
-	// 	question: "Epithelium that appears layered due to the varying levels at which nuclei are found in cells, but in reality is not layered, is _________________.",
-	// 	choices: ["transitional epithelium", "pseudostratified columnar epithelium", "stratified squamous epithelium", "stratified columnar epithelium"],
-	// 	correctAnswer:1
-	// },
-	// {
-	// 	question: "The outer layer of the skin is composed of ______________________.",
-	// 	choices: ["transitional epithelium", "pseudostratified columnar epithelium", "stratified squamous epithelium", "stratified columnar epithelium"],
-	// 	correctAnswer:2
-	// },
-	// {
-	// 	question: "The primary purpose of stratification, or layering, in epithelial tissue is for increased _____________.",
-	// 	choices: ["protection", "secretion", "absorption", "thickening of the basement membrane"],
-	// 	correctAnswer:0
-	// },
-	// {
-	// 	question: "What type of epithelium lines the urinary bladder and is capable of distention?",
-	// 	choices: ["stratified cuboidal epithelium", "stratified squamous epithelium", "transitional epithelium", "stratified columnar epithelium"],
-	// 	correctAnswer:2
-	// },
+	{
+		question: "Epithelium that appears layered due to the varying levels at which nuclei are found in cells, but in reality is not layered, is _________________.",
+		choices: ["transitional epithelium", "pseudostratified columnar epithelium", "stratified squamous epithelium", "stratified columnar epithelium"],
+		correctAnswer:1
+	},
+	{
+		question: "The outer layer of the skin is composed of ______________________.",
+		choices: ["transitional epithelium", "pseudostratified columnar epithelium", "stratified squamous epithelium", "stratified columnar epithelium"],
+		correctAnswer:2
+	},
+	{
+		question: "The primary purpose of stratification, or layering, in epithelial tissue is for increased _____________.",
+		choices: ["protection", "secretion", "absorption", "thickening of the basement membrane"],
+		correctAnswer:0
+	},
+	{
+		question: "What type of epithelium lines the urinary bladder and is capable of distention?",
+		choices: ["stratified cuboidal epithelium", "stratified squamous epithelium", "transitional epithelium", "stratified columnar epithelium"],
+		correctAnswer:2
+	},
 	{
 		question: "An exocrine gland that loses small parts of its cell bodies during secretion, as is the case for the mammary gland, is further classified as a(n) ____________ gland.",
 		choices: ["merocrine", "apocrine", "holocrine", "endocrine"],
@@ -43,8 +43,8 @@ var html = "<div>";
    		for (var i = 0; i < quiz.length; i++) {
     		html+="<li data-questions-index=" + i + " class=individualQuestion><h4>"+quiz[i].question+"</h4>";
 			for(var j = 0; j < quiz[i].choices.length; j++) {
-				html+="<input type = radio name=quizChoices_" + i + " value=" + j + "> " + quiz[i].choices[j]+ "</input><br>";
-				// console.log("quiz choices", quiz[i].choices[j]);
+				html+="<input class=radioOptions type = radio name=quizChoices_" + i + " value=" + j + "> " + quiz[i].choices[j]+ "</input><br>";
+				//console.log("quiz choices", quiz[i].choices[j].checked);
 				$("#quizItem_" + i + " input").on('click', function(){
 					console.log("a value", $(this).val());
 				});
@@ -56,26 +56,54 @@ var html = "<div>";
 html+="</div>";
 document.getElementById("questions").innerHTML = html;
 
+var totalQuestions = $('.individualQuestion').length;
+var totalCorrect = 0;
 $('.individualQuestion input').on('click', function() {
 	var selectedAnswer = parseInt($(this).val()),
 		questionIndex = $(this).closest('li').data('questions-index'),
 		correctAnswer = quiz[questionIndex].correctAnswer;
+		
+		console.log("selectedAnswer", selectedAnswer);
+		console.log("questionIndex", questionIndex);
+		console.log("correctAnswer", correctAnswer);
+		console.log("input", $('.individualQuestion').val());
+		
+    
 	if(selectedAnswer === correctAnswer){
-		console.log('true');
+	  	totalCorrect ++;
+		document.getElementById('score').innerHTML = totalCorrect + "/ " + totalQuestions + " " + Math.floor(totalCorrect/totalQuestions * 100) + "%";
+	    console.log('total correct', totalCorrect);
+		console.log('correct');
 	} else {
 		console.log('false');
 	}
+	return selectedAnswer;
 });
 
 var counter = 1;
+console.log(totalQuestions);
+if(counter < totalQuestions){
+	$('#next').show();
+} else if (counter === totalQuestions) {
+	$('#next').hide();
+}
 var radioCheck = document.getElementsByName('quizChoices_i');
 $(".individualQuestion:nth-child(" + counter + ")").fadeIn('slow');
-$("#next").click(function(){
-	if(counter < quiz.length) {
-		itemSelected = true;
+$('#submit').hide();
+$("#next").click(function(event){
+	var answerSelected = $('.radioOptions').is(':checked');
+	console.log("is answer selected", answerSelected);
+	if(answerSelected === true && counter < quiz.length) {
+		// console.log(radioCheck);
 		counter++; 
+		$('#submit').hide();
 		$(".individualQuestion").hide();
 		$(".individualQuestion:nth-child(" + counter + ")").fadeIn('slow');
+	} else if(answerSelected === true && counter === quiz.length) {
+		$('#submit').show();
+	} else {
+		event.preventDefault();
+		alert("You must select an answer before moving to the next question.");
 	}
 });
 $("#previous").click(function(){
@@ -83,7 +111,8 @@ $("#previous").click(function(){
 		counter--; 
 		$(".individualQuestion").hide();
 		$(".individualQuestion:nth-child(" + counter + ")").fadeIn('slow');
-	}
+		$('#submit').hide();
+	};
 });
 
 
